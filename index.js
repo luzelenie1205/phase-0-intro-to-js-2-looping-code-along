ea1e24654b8d7c83549f1809817a641efd24daf1
luzelenie <luzelenie1205@gmail.com>
2021-07-23 13:51:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const newArray=[];
+
+function writeCards(stringNames,eventName){
+    for (let i=0; i< stringNames.length;i++){
+       
+newArray.push("Thank you, "+ stringNames[i]+", for the wonderful surprise gift!")
+    }
+    return newArray
+}
+
+function  countDown(numbers){
+ let i=11;
+ while (i>0){
+     console.log(numbers--)
+     i--
+ }
+return numbers;
+}
